c7de219dc64fae391fe736c5c9d614b1f0c9a387
sziniuk-blackbird <sziniuk@blackbird-lab.com>
2020-03-24 14:24:43 +0200

    intentionally failing test

--- src/App.test.js
+++ src/App.test.js
@@ -4,6 +4,10 @@ import App from './App';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<App/>, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+it('should fail', () => {
+  expect(1).toEqual(2);
 });
